20c8b5211af49146fad66bf9613684d67de54165
wangclare <wangzeyang_hfut@163.com>
2025-05-14 13:31:23 +0200

    get the last epoch encoder

--- bracs_extractor.sh
+++ bracs_extractor.sh
@@ -5,10 +5,10 @@
 #SBATCH --partition=gpu_p100
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --gpus-per-node=1
-#SBATCH --mem=48G
-#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --gpus-per-node=2
+#SBATCH --mem=45G
+#SBATCH --time=06:00:00
 #SBATCH --output=logs/bracs_extractor_%j.out
 #SBATCH --error=logs/bracs_extractor_%j.err
 #SBATCH --mail-type=END,FAIL
@@ -24,9 +24,13 @@ echo "CMD: $0 $@"
 
 source /data/leuven/373/vsc37341/miniconda3/etc/profile.d/conda.sh
 conda activate wsi_env
-
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export HYDRA_FULL_ERROR=1
 cd /data/leuven/373/vsc37341/wsi_code
 
-python /data/leuven/373/vsc37341/solo-learn/main_pretrain.py --config-path /data/leuven/373/vsc37341/wsi_code --config-name bracs_simclr
+python -m torch.distributed.run --nproc_per_node=2 \
+  /data/leuven/373/vsc37341/solo-learn/main_pretrain.py \
+  --config-path /data/leuven/373/vsc37341/wsi_code \
+  --config-name bracs_simclr
 
 echo "✅ Finished at: $(date)"
